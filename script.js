08557f530985300c63174dd63bdca0dcd7ca3b6d
Jhullya2720 <108037255+Jhullya2720@users.noreply.github.com>
2023-09-12 20:35:32 -0300

    Tarefa Finalizada

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  str = str.replace(/\s/g, '').toLowerCase();
+
+  let start = 0;
+  let end = str.length - 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  if (arr.length == 0) {
+    return [];
+  }
+  let min = arr[0]
+  let max = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  let resultado = [min, max];
+  return resultado
 }
+isPalindrome()
+arrayMaxMin()
